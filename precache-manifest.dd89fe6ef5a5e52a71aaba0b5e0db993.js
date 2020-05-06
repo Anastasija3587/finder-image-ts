3a754740b0984656eb2e74a6f752a6248a8de332
Anastasija3587 <anastasija3587@gmail.com>
2020-05-06 13:44:12 +0300

    Updates

---
+++ precache-manifest.dd89fe6ef5a5e52a71aaba0b5e0db993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3597927770d803cfbb9eb218668faf",
+    "url": "/finder-image-ts/index.html"
+  },
+  {
+    "revision": "5d4b4d777879c6d8c61c",
+    "url": "/finder-image-ts/static/css/main.65a0abd5.chunk.css"
+  },
+  {
+    "revision": "5ae35e298c84cfc5e3b7",
+    "url": "/finder-image-ts/static/js/2.1d696463.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/finder-image-ts/static/js/2.1d696463.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d4b4d777879c6d8c61c",
+    "url": "/finder-image-ts/static/js/main.70cf1b5c.chunk.js"
+  },
+  {
+    "revision": "84fdd4337ad9b43e8ffd",
+    "url": "/finder-image-ts/static/js/runtime-main.50c37953.js"
+  }
+]);
